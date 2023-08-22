8b21d6172e502345266540dcd3512c641fe71289
Fnige <35159947+Fnige@users.noreply.github.com>
2023-08-22 11:40:27 +1000

    Fixed indentation hopefully

--- flake.nix
+++ flake.nix
@@ -5,33 +5,33 @@
 		# Specify the source of Home Manager and Nixpkgs.
 		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    # Home Manager
-    home-manager = {
+	# Home Manager
+	home-manager = {
 			url = "github:nix-community/home-manager";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 
-    # Flake Parts
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+	# Flake Parts
+	flake-parts = {
+		url = "github:hercules-ci/flake-parts";
+		inputs.nixpkgs.follows = "nixpkgs";
+	};
 	};
 
 	outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-    
+
 		nixosConfigurations."yawre-mother" = nixpkgs.lib.nixosSystem {
 			system = "x86_64-linux";
 			specialArgs = inputs;
 
 			modules = [
 				./configuration.nix
-        home-manager.nixosModules.home-manager
-          {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.fnige = import ./home.nix;
-        }
+				home-manager.nixosModules.home-manager
+				{
+				home-manager.useGlobalPkgs = true;
+				home-manager.useUserPackages = true;
+				home-manager.users.fnige = import ./home.nix;
+				}
 			];
 		};
 	};
